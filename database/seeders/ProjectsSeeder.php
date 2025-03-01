5d3d557cc9a5af70b08d681e9496182093972f76
Stefan Hoferichter <hoferich@gmx.de>
2025-03-01 12:37:30 +0100

    seeder contains only sample data

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -13,24 +13,9 @@ class ProjectsSeeder extends Seeder
     public function run(): void
     {
         DB::table('projects')->insert([
-            'title' => 'RezeptExperte.de',
-            'sitemap' => 'https://www.rezeptexperte.de/sitemap.xml',
-            'language_id' => '1',
-        ]);
-        DB::table('projects')->insert([
-            'title' => 'MedPort.de',
-            'sitemap' => 'https://www.medport.de/sitemap.xml',
-            'language_id' => '1',
-        ]);
-        DB::table('projects')->insert([
             'title' => 'Hoferichter.net',
             'sitemap' => 'https://www.hoferichter.net/sitemap.xml',
             'language_id' => '1',
-        ]);
-        DB::table('projects')->insert([
-            'title' => 'Aspell.net',
-            'sitemap' => 'http://aspellxxx/sitemap.xml',
-            'language_id' => '2',
         ]);
     }
 }
